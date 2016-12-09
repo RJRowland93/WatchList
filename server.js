ff33db9419dd163279a7b82fc30bb3c23416928a
RJRowland93 <r.rowland93@gmail.com>
2016-12-09 13:22:25 -0600

    linked up JawsDB connection

--- server.js
+++ server.js
@@ -22,12 +22,18 @@ app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "movie_planner_db"
-});
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
